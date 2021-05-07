9115789e3c341e56a02371eb174de9f667150574
ARI3L <66218989+ARI3LUnet@users.noreply.github.com>
2021-05-07 18:07:06 -0300

    Update gnuworld.sh

--- gnuworld.sh
+++ gnuworld.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-cd gnuworld
-cd bin
+cd /home/gnuworld/gnuworld/bin
 /usr/local/pgsql/bin/postmaster -B 64 -N 32 -i -D /usr/local/pgsql/data -o -F -h 127.0.0.1 >/dev/null 2>&1 &
 read -t 7 -rsp $'Wait 7 secs...\n' 
 ./gnuworld -f GNUWorld.conf
